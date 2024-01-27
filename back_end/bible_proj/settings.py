dd1aa129a6d5a12bc3c11a66cd27dab26ab7d008
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-27 16:53:14 -0600

    changes to keys

--- back_end/bible_proj/settings.py
+++ back_end/bible_proj/settings.py
@@ -13,22 +13,23 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from dotenv import dotenv_values
 
-env = dotenv_values(".env")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = dotenv_values(".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3z!dm4(x!idbw^*&%6^&kfdz8*ui141w!q=^l5dg0*2l6un4!$'
+SECRET_KEY = env.get("SECRET_KEY")
+
+# os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
