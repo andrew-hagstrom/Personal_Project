094d462c8d1c34ef62a9ef70f5b1865cd68b5523
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-27 22:10:29 -0600

    url

--- front_end/bible-project/src/utilities.jsx
+++ front_end/bible-project/src/utilities.jsx
@@ -13,6 +13,6 @@ export const api = axios.create({
 // api.get(${ROUTES.user/login})
 
 export const userapi = axios.create({
-    baseURL: "http://18.119.29.248/api/v1/users/"
+    baseURL: 'http://18.119.29.248/api/v1/users/'
     // baseURL: "http://127.0.0.1:8000/api/v1/users"
 });
