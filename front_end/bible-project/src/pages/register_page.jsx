7480bd229f9c7a5994f5e11094c39485446301a0
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-28 15:36:36 -0600

    change

--- front_end/bible-project/src/pages/register_page.jsx
+++ front_end/bible-project/src/pages/register_page.jsx
@@ -23,30 +23,24 @@ export const Register = () => {
         console.log(data)
         const response = await api
             .post("users/signup/", data)
-            // .catch(err => console.log(`signup err ${err}`));
+            .catch(err => console.log(`signup err ${err}`));
             console.log("Response:", response);
         
-        if (response && response.data) {
-            const userEmail = response.data.email; 
-            const token = response.data.token;
-            
-            console.log(`signup success, email: ${userEmail}, token: ${token}`);
+        
+        const userEmail = response.data.email; 
+        const token = response.data.token;
 
-            api.defaults.headers.common["Authorization"] = `Token ${token}`
-            localStorage.setItem("token", token);
-            localStorage.setItem("email", userEmail);
-            
-            setUser(True)
+
+        console.log(`signup success, email: ${userEmail}, token: ${token}`);
+
+        api.defaults.headers.common["Authorization"] = `Token ${token}`
+        localStorage.setItem("token", token);
+        localStorage.setItem("email", userEmail);
+        
+        setUser(True)
        
        
-    } else {
-        console.log('Response is undefined or does not contain data:', response);
     }
-    
-};
-
-
-
 
     const handleLoginClick = () => {
         navigate("/login/"); 
