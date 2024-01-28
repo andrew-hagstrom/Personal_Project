769cd3b864eb4083372913870ebbd4559dbdf031
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-28 16:51:15 -0600

    dsdsd

--- front_end/bible-project/src/pages/register_page.jsx
+++ front_end/bible-project/src/pages/register_page.jsx
@@ -22,7 +22,7 @@ export const Register = () => {
         console.log('hello')
         console.log('Request Payload:', data);
         const response = await userapi
-            .post("signup/", data)
+            .post("signup", data)
             .catch(err => console.log(`signup err ${err}`));
             console.log("Response:", response);
         
