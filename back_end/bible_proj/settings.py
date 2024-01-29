c407b99337b25363f83aac2276da94a01ed63780
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-29 00:57:19 -0600

    fd

--- back_end/bible_proj/settings.py
+++ back_end/bible_proj/settings.py
@@ -17,8 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = dotenv_values(".env")
 
-SECRET_KEY = env.get("SECRET_KEY")
-# os.environ.get("SECRET_KEY")
+# SECRET_KEY = env.get("SECRET_KEY")
+SECRET_KEY=os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
