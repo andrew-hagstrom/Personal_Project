9f1ec97dbd7fc1733d8d7157a46d71fa500cd7f0
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-28 23:39:30 -0600

    fddf

--- back_end/bible_proj/settings.py
+++ back_end/bible_proj/settings.py
@@ -12,17 +12,20 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from dotenv import dotenv_values
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = dotenv_values(".env")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+env = dotenv_values(".env")
 
 # os.environ.get("SECRET_KEY")
 
