4863c91a53eaf53af3c24fab77c5cfc2a9086e8e
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-30 01:17:01 -0600

    api

--- front_end/bible-project/src/pages/word_page.jsx
+++ front_end/bible-project/src/pages/word_page.jsx
@@ -1,6 +1,5 @@
 
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { useParams, useLocation } from 'react-router-dom';
 import { useNavigate  } from 'react-router-dom';
 import '../App.css';
