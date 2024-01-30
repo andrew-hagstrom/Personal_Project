ae671190770ad1d990e009eadae57fc46b7873ae
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-30 01:58:17 -0600

    message data

--- front_end/bible-project/src/pages/word_page.jsx
+++ front_end/bible-project/src/pages/word_page.jsx
@@ -28,7 +28,7 @@ const WordPage = () => {
         let token = localStorage.getItem("token")
         api.defaults.headers.common['Authorization'] = `Token ${token}`;
         const response = await api.get(`word/${word}/`);
-        setWordData(response.data[1]);
+        setWordData(response.data);
         // setWordData(response.data[1][1][0][2][1][0][1]);
         console.log(data)
         setMorphologyLoaded(true)
