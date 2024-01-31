7bbca10ce95493ed96aee5c6b8bef99a0d79d7a7
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-30 21:53:59 -0600

    dsd

--- front_end/bible-project/src/pages/word_page.jsx
+++ front_end/bible-project/src/pages/word_page.jsx
@@ -30,7 +30,6 @@ const WordPage = () => {
         const response = await api.get(`word/${word}/`);
         setWordData(response.data[1][1][0][3][1][0][1]);
         // setWordData(response.data[1][1][0][2][1][0][1]);
-        setIsLoading(false);
         setMorphologyLoaded(true)
       } catch (error) {
         console.error('Error fetching word data:', error);
